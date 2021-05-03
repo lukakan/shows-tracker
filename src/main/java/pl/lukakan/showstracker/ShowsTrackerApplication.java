62b6b585e6e8597a14e4bf77af7f4a5abcb1c503
Lukasz K <kanicki.lukasz@gmail.com>
2021-05-03 19:09:00 +0200

    initial commit

---
+++ src/main/java/pl/lukakan/showstracker/ShowsTrackerApplication.java
@@ -0,0 +1,13 @@
+package pl.lukakan.showstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowsTrackerApplication.class, args);
+    }
+
+}
